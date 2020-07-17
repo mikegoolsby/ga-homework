c1cddaede64ef5aa3377f9613115ad58483aa444
Michael Goolsby <mgoolsby1091@gmail.com>
2020-07-17 13:38:25 -0400

    Excited Kittens answered

--- unit_1/w02_d02/answ.js
+++ unit_1/w02_d02/answ.js
@@ -63,17 +63,44 @@
 // Methods, Revisited
 ////////////////////////////////
 
-const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
-console.log(favMovies.indexOf('Titanic'));
-favMovies.sort();
-favMovies.pop();
-favMovies.push('Guardians of the Galaxy');
-favMovies.reverse();
-favMovies.shift();
-favMovies.unshift();
-console.log(favMovies.indexOf('Django Unchained'));
-favMovies.splice(14, 1, 'Avatar');
-let progSlice = favMovies.slice(favMovies.length/2);
-console.log(progSlice);
-console.log(favMovies);
-console.log(favMovies.indexOf('Django Unchained')); // homework states we should console.log the index of fast and furious but we did not remove this from the array - we did however remove django unchained and get a value of -1 as its index
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// console.log(favMovies.indexOf('Titanic'));
+// favMovies.sort();
+// favMovies.pop();
+// favMovies.push('Guardians of the Galaxy');
+// favMovies.reverse();
+// favMovies.shift();
+// favMovies.unshift();
+// console.log(favMovies.indexOf('Django Unchained'));
+// favMovies.splice(14, 1, 'Avatar');
+// let progSlice = favMovies.slice(favMovies.length/2);
+// console.log(progSlice);
+// console.log(favMovies);
+// console.log(favMovies.indexOf('Django Unchained')); // homework states we should console.log the index of fast and furious but we did not remove this from the array - we did however remove django unchained and get a value of -1 as its index
+
+
+////////////////////////////////
+// Where is Waldo
+////////////////////////////////
+
+// const whereIsWaldo = [["Timmy", "Frank"], "Eggbert", ["Lucinda", "Jacc", "Neff", "Snoop"], ["Petunia", ["Baked Goods", "Waldo"]]];
+// whereIsWaldo.splice([1][0], 1);
+// whereIsWaldo[1][2] = 'No One';
+// console.log(whereIsWaldo[2][1][1]);
+
+////////////////////////////////
+//  Excited Kitten
+////////////////////////////////
+
+const talkingKitten = ["Love me, pet me! HSSSSSS!", "...human...why you taking pictures of me?...", "...why does the red dot always get away..."];
+let meow = 0;
+
+for (i=0; i<21; i++) {
+    if (i % 2 === 0) {
+        console.log(talkingKitten[meow]);
+    } else (i % 2 !== 0); {
+        let purr = Math.random() * (talkingKitten.length);
+        let purring = Math.round(purr)
+        console.log(talkingKitten[purring]);
+    }
+}
