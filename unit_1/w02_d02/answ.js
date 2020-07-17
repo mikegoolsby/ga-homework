960acd36c8c21dae19ccf280145116539af848f4
Michael Goolsby <mgoolsby1091@gmail.com>
2020-07-17 11:55:15 -0400

    Wild Wild Life answered

--- unit_1/w02_d02/answ.js
+++ unit_1/w02_d02/answ.js
@@ -6,14 +6,30 @@
 //     console.log(i)
 // }
 
-for (i=1; i<101; i++) {
-    if (i % 3 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if (i % 3 === 0) {
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(i);
-    }
-}
+// for (i=1; i<101; i++) {
+//     if (i % 3 && i % 5 === 0) {
+//         console.log('FizzBuzz');
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] = 5001
+// console.log(plantee);
+wolfy[3] = 'Gotham City'
+// console.log(wolfy);
+dart.push('Hawkins');
+// console.log(dart);
+wolfy.shift();
+wolfy.unshift('Gameboy');
+console.log(wolfy);
