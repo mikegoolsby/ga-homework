bc128899b55dea458a1f3d032147ec0231a285a2
Mike Goolsby <mikegoolsby@mikes-macbook.myfiosgateway.com>
2020-07-19 14:42:29 -0400

    verbal questions done

--- unit_1/w02_d03/solutions.js
+++ unit_1/w02_d03/solutions.js
@@ -3,3 +3,7 @@
 // .. specifying the value of the parameter when invoking the function is
 // .. called an argument
 
+// Within a function, what is the difference between return and console.log?
+// .. in order for a function to work, it needs to have a return 
+// .. output cannot come from a console.log - return values will not appear
+// .. in the console unless they are console.log'd
