0539bea58c6ea78510f1f734b739a8a9d54042d4
Mike Goolsby <mikegoolsby@mikes-macbook.myfiosgateway.com>
2020-07-19 14:38:43 -0400

    verbal q's completed

---
+++ unit_1/w02_d03/solutions.js
@@ -0,0 +1,5 @@
+// What is the difference between a parameter and an argument?
+// .. A paramater is an input to a function that can modify its behavior
+// .. specifying the value of the parameter when invoking the function is
+// .. called an argument
+
