155ee4d4ef21b46539811745e89b201209a7e2cc
Mike Goolsby <mikegoolsby@mikes-macbook.myfiosgateway.com>
2020-07-19 20:59:15 -0400

    accumulator answered

--- unit_1/w02_d03/solutions.js
+++ unit_1/w02_d03/solutions.js
@@ -20,5 +20,15 @@ const checkPalindrome = (word) => {
   }
 }
 
-console.log(checkPalindrome('radar'));
-console.log(checkPalindrome('Borscht'));
+// console.log(checkPalindrome('radar'));
+// console.log(checkPalindrome('Borscht'));
+
+
+const sumArray = (nums) => {
+  let sum = 0
+  for (i=0; i<nums.length; i++) {
+      sum += nums[i];
+  } return sum
+}
+
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
