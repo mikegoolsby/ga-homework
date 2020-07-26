d80af2434bdf756ffd7120e71809428c8fb977f8
Michael Goolsby <mgoolsby1091@gmail.com>
2020-07-26 18:04:03 -0400

    finishes landscaper game

--- unit_1/w03_d02/app.js
+++ unit_1/w03_d02/app.js
@@ -2,12 +2,15 @@ let wallet = 0;
 let tool = 'teeth';
 
 const cutLawn = () => {
-  let firstAnswer = prompt('Time to landscape. What will it be?', "'Cut the lawn' or 'Quit'?")
-  if (firstAnswer === 'Cut the lawn' && wallet < 5) {
+  let firstAnswer = prompt('Time to landscape. What will it be?', "'Cut the lawn', 'New tool', or 'Quit'?")
+  if (firstAnswer === 'Cut the lawn') {
       wallet++
-      alert(`You cut the lawn with the tool: ${tool}. You now have ${wallet} dollars in your wallet.`)
+      alert(`You cut the lawn with the tool: ${tool}. You now have $${wallet} in your wallet. It costs $5 for rusty scissors.`)
       cutLawn();
-  } else if (wallet == 5) {
+  } else if (firstAnswer === 'New tool' && wallet < 5) {
+    alert("You've been working hard lately.. but not hard enough. Keep working!")
+    cutLawn();
+  } else if (firstAnswer === 'New tool' && wallet >= 5) {
     newTool();
   } else {
     alert('Bye!')
@@ -16,17 +19,21 @@ const cutLawn = () => {
 
 const newTool = () => {
   tool = 'rusty scissors'
-  alert(`Congrats! You now have ${wallet} dollars and you've earned a new tool: ${tool}!`)
+  wallet-=5
+  alert(`Congrats! You now have $${wallet} and you've purchased a new tool: ${tool}!`)
   keepCutting();
 }
 
 const keepCutting = () => {
-  let secondAnswer = prompt('Time to landscape. What will it be?', "'Cut the lawn' or 'Quit'?")
-  if (secondAnswer === 'Cut the lawn' && wallet < 25) {
+  let secondAnswer = prompt('Time to landscape. What will it be?', "Cut the lawn', 'New tool', or 'Quit'?")
+  if (secondAnswer === 'Cut the lawn') {
     wallet+=5
-    alert(`You cut the lawn with the tool: ${tool}. You now have ${wallet} dollars in your wallet.`)
+    alert(`You cut the lawn with the tool: ${tool}. You now have $${wallet} dollars in your wallet. It costs $25 for a push lawnmower.`)
     keepCutting();
-  } else if (wallet == 25) {
+  } else if (secondAnswer === 'New tool' && wallet < 25) {
+    alert("You've been working hard lately.. but not hard enough. Keep working!")
+    keepCutting();
+  } else if (secondAnswer === 'New tool' && wallet >= 25) {
     newToolAgain();
   } else {
     alert('Bye!')
@@ -35,17 +42,21 @@ const keepCutting = () => {
 
 const newToolAgain = () => {
   tool = 'push lawnmower'
-  alert(`Congrats! You now have ${wallet} dollars and you've earned a new tool: ${tool}!`)
+  wallet-=25
+  alert(`Congrats! You now have $${wallet} and you've purchased a new tool: ${tool}!`)
   cutForMoney();
 }
 
 const cutForMoney = () => {
-  let thirdAnswer = prompt('Time to landscape. What will it be?', "'Cut the lawn' or 'Quit'?")
-  if (thirdAnswer === 'Cut the lawn' && wallet < 250) {
+  let thirdAnswer = prompt('Time to landscape. What will it be?', "Cut the lawn', 'New tool', or 'Quit'?")
+  if (thirdAnswer === 'Cut the lawn') {
     wallet+=50
-    alert(`You cut the lawn with the tool: ${tool}. You now have ${wallet} dollars in your wallet.`)
+    alert(`You cut the lawn with the tool: ${tool}. You now have $${wallet} in your wallet. It costs $250 for a battery-powered lawnmower.`)
     cutForMoney();
-  } else if (wallet >= 250) {
+  } else if (thirdAnswer === 'New tool' && wallet < 250) {
+    alert("You've been working hard lately.. but not hard enough. Keep working!")
+    cutForMoney();
+  } else if (thirdAnswer === 'New tool' && wallet >= 250) {
     newToolBattMower();
   } else {
     alert('Bye!')
@@ -54,17 +65,21 @@ const cutForMoney = () => {
 
 const newToolBattMower = () => {
   tool = 'battery-powered lawnmower'
-  alert(`Congrats! You now have ${wallet} dollars (which is more than the $250 price of the fancy lawnmower) and you've earned a new tool: ${tool}!`)
+  wallet-=250
+  alert(`Congrats! You now have $${wallet} and you've purchased a new tool: ${tool}!`)
   cutForBigMoney();
 }
 
 const cutForBigMoney = () => {
-  let fourthAnswer = prompt('Time to landscape. What will it be?', "'Cut the lawn' or 'Quit'?")
-  if (fourthAnswer === 'Cut the lawn' && wallet < 500) {
+  let fourthAnswer = prompt('Time to landscape. What will it be?', "Cut the lawn', 'New tool', or 'Quit'?")
+  if (fourthAnswer === 'Cut the lawn') {
     wallet+=100
-    alert(`You cut the lawn with the tool: ${tool}. You now have ${wallet} dollars in your wallet.`)
+    alert(`You cut the lawn with the tool: ${tool}. You now have $${wallet} in your wallet. It costs $500 to hire a team of starving students.`)
     cutForBigMoney();
-  } else if (wallet >= 500) {
+  } else if (fourthAnswer === 'New tool' && wallet < 500) {
+    alert("You've been working hard lately.. but not hard enough. Keep working!")
+    cutForBigMoney();
+  } else if (fourthAnswer === 'New tool' && wallet >= 500) {
     starvingStudents();
   } else {
     alert('Bye!')
@@ -73,17 +88,21 @@ const cutForBigMoney = () => {
 
 const starvingStudents = () => {
   tool = 'a team of starving students'
-  alert(`Congrats! You now have ${wallet} dollars and you've earned (maybe 'hired' is a better word) a new tool: ${tool}!`)
+  wallet-=500
+  alert(`Congrats! You now have $${wallet} and you've purchased (maybe 'hired' is a better word) a new tool: ${tool}!`)
   finalStretch();
 }
 
 const finalStretch = () => {
-  let fifthAnswer = prompt('Time to landscape. What will it be?', "'Cut the lawn' or 'Quit'?")
-  if (fifthAnswer === 'Cut the lawn' && wallet < 1000) {
+  let fifthAnswer = prompt('Time to landscape. What will it be?', "Cut the lawn', 'New tool', or 'Quit'?")
+  if (fifthAnswer === 'Cut the lawn') {
     wallet+=250
-    alert(`You cut the lawn with the tool: ${tool}. You now have ${wallet} dollars in your wallet.`)
+    alert(`You cut the lawn with the tool: ${tool}. You now have ${wallet} dollars in your wallet. Once you have $1000, you win! Keep mowing those lawns!`)
     finalStretch();
-  } else if (wallet >= 1000) {
+  } else if (fifthAnswer === 'New tool' && wallet < 1000) {
+    alert("You've been working hard lately.. but not hard enough. Keep working!")
+    finalStretch();
+  } else if (fifthAnswer === 'New tool' && wallet >= 1000) {
     victoryLap();
   } else {
     alert('Bye!')
