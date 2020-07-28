345c81df559e4f32f35ca433dd8ca193dc8ed0e4
Mike Goolsby <mikegoolsby@mikes-macbook.myfiosgateway.com>
2020-07-28 10:26:27 -0400

    answers verbal questions

---
+++ unit_1/w03_d03/answers.js
@@ -0,0 +1,21 @@
+// A light switch that can be either on or off.
+//// boolean -- let switch = true;
+// A user's email address.
+//// string -- emailAddress = 'mgoolsby1091@gmail.com';
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// array -- let spaceship = ['hull', 'laser blasters', 'tractor beam', 'warp drive']
+// A list of student names from our class.
+//// array -- let studentNames = [..."names"]
+// A list of student names from our class, each with a location.
+//// object-- const studentNames = {
+    // larry: "new hampshire"
+// }
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+//// array, within an object, with an object, where the list of tv shows is an array-- 
+//// const studentNames = {
+//     larry: {
+//         location: "new hampshire"
+//         tvShows: ['here']
+//     }
+// }
+
