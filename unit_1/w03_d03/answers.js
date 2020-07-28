f5b5f60e6e44597c9d5b88786598237d9f235ca1
Mike Goolsby <mgoolsby1091@gmail.com>
2020-07-28 11:33:52 -0400

    objectception complete

--- unit_1/w03_d03/answers.js
+++ unit_1/w03_d03/answers.js
@@ -65,3 +65,25 @@ const crazyObject = {
 // console.log(Object.keys(crazyObject.larry)[2])
 // crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk")
 // console.log(crazyObject.larry.quotes)
+
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ }
+
+ inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+
+ console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
