0f5f0d5d970d4ba9de320d3a1b7c42af112cb2e5
Michael Goolsby <mgoolsby1091@gmail.com>
2020-08-02 17:11:20 -0400

    updates w4d2 hw progress

--- unit_1/w04_d02/combining_datatypes.js
+++ unit_1/w04_d02/combining_datatypes.js
@@ -53,3 +53,69 @@ const powerButton = () => {
 }
 
 // powerButton();
+
+//////////////////////
+
+const vendingMachine = {
+    snacks: [{name: 'Reeses',
+        price: '2.50'}, 
+        {name: 'Granola Bar', 
+        price: '2.50'}, 
+        {name: 'Chips',
+        price: '3.00'
+    }],
+    vend: (index) => {
+        console.log(vendingMachine.snacks[index].name)
+    }
+}
+
+// vendingMachine.vend(2);
+
+////////////////////////
+
+const add = (num1, num2) => {
+  return num1 + num2
+}
+
+const subtract = (num1, num2) => {
+  return num1 - num2
+}
+
+const multiply = (num1, num2) => {
+  return num1 * num2
+}
+
+const divide = (num1, num2) => {
+  return num1 / num2
+}
+
+const calculate = (num1, num2, operates) => {
+  return operates(num1, num2)
+}
+
+// console.log(calculate(2, 2, add));
+// console.log(calculate(2, 2, subtract));
+// console.log(calculate(2, 2, multiply));
+// console.log(calculate(2, 2, divide));
+
+////////////////////////
+
+
+const bar = () => {
+    console.log('bar here');
+}
+bar(); // have to invoke functions in the proper spot
+
+const foo = () => {
+    console.log('foo here');
+}
+foo(); // same here
+
+// foo(); // you can't invoke a function before you define it
+
+// const foo ()=>{
+//     console.log('hi');
+//
+
+////////////////////////
+
