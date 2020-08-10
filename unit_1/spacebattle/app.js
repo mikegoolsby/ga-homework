9e4c8da5b7c3e8ff84bbd7e6a27279d7004241a0
Michael Goolsby <mgoolsby1091@gmail.com>
2020-08-10 13:25:05 -0400

    finishes spacebattle

--- unit_1/spacebattle/app.js
+++ unit_1/spacebattle/app.js
@@ -1,24 +1,4 @@
-const ussSpaceCat = {
-    hull: 20,
-    firepower: 5,
-    accuracy: .7,
-    attack: (alien) => {
-      if (Math.random() <= alien.accuracy) {
-            alien.hull-=this.firepower
-            if (Number.isNaN(alien.hull)) {
-                console.log(`Space cat attack successful! Alien ship is destroyed!`);
-            } else {
-                console.log(`Space cat attack successful! Alien ship has ${alien.hull} health points left!`)
-            }
-      } else {
-          console.log(`Space Cat attack wasn\'t successful!`)
-      }
-    },
-    retreat: () => {
-        alert('Peace was not achieved this time.. thanks for playing!')
-    }
-}
-
+// Defines Alien class
 class Alien {
     constructor() {
         this.hull = Math.floor(Math.random() * 4) + 3;
@@ -29,7 +9,7 @@ class Alien {
         if (Math.random() <= ship.accuracy) {
             ship.hull-=this.firepower;
             if (Number.isNaN(ship.hull)) {
-                console.log('Alient attack successful! Space cat ship is destroyed! GAME OVER!')
+                console.log('Alien attack successful! Space cat ship is destroyed! GAME OVER!')
             } else {
                 console.log(`Alien attack successful! Space cat has ${ship.hull} health points left!`);
             }
@@ -45,32 +25,59 @@ for (let i=0; i<6; i++) {
     alienShips.push(new Alien)
 }
 
-// Beginning dialogue in the form of browser alerts
-alert("Welcome to SPACEBATTLE! The timeless classic console space adventure between space foes.")
-alert(`Captain Space Cat... There are ${alienShips.length} alien ships headed your way! Start reading your browser console and ready up for battle!`)
-
-// Prompt to capture first user input (maybe abstract this into a function?)
-const initialInput = prompt("Atack, or retreat?", "'Attack', 'Retreat'");
-
-
-let alien = alienShips[0]
-if (initialInput.toLowerCase() === 'attack') {
-    ussSpaceCat.attack(alien);
-} else {
-    ussSpaceCat.retreat();
+const mainAction = () => {
+    const initialInput = prompt("Atack, or retreat?", "'Attack', 'Retreat'");
+    let alien = alienShips[0]
+    if (initialInput.toLowerCase() === 'attack') {
+        ussSpaceCat.attack(alien);
+    } else {
+        ussSpaceCat.retreat();
+    }
 }
 
-// if (alien.hull >= 0) {
-//     alien.attack(ussSpaceCat)
-// } else {
-//     initialInput();
-// }
+const gameWon = () => { // if the aliens are gone, ie array is empty, you win
+    if (alienShips.length > 0) {
+        mainAction();
+    } else {
+        console.log("Congratulations, Captain. The aliens have been defeated. Peace has been restored. Let's get some pizza.")
+        ussSpaceCat.victory();
+    }
+  }
 
-// Assigns alien to the first index in the Alien Ships array, then attacks the first ship up
-// let alien = alienShips[0]
-// ussSpaceCat.attack(alien);
+// Space Cat, or hero, object
+const ussSpaceCat = {
+    hull: 20,
+    firepower: 5,
+    accuracy: .7,
+    attack: (alien) => {
+      if (Math.random() <= alien.accuracy) {
+            alien.hull-=this.firepower
+            if (Number.isNaN(alien.hull)) {
+                console.log(`Space cat attack successful! Alien ship is destroyed!`);
+                alienShips.shift();
+                console.log(`There are now ${alienShips.length} ships left!`)
+                gameWon();
+            } else {
+                console.log(`Space cat attack successful! Alien ship has ${alien.hull} health points left!`)
+            }
+      } else {
+          console.log(`Space Cat attack wasn\'t successful!`)
+          gameWon();
+      }
+    },
+    retreat: () => {
+        alert('Peace wasn\'t achieved this time... Thanks for playing!')
+    },
+    victory: () => {
+        alert('Thanks for playing!')
+    }
+}
 
-// alien1.attack(ussSpaceCat);
 
+// Beginning dialogue in the form of browser alerts
+alert("Welcome to SPACEBATTLE! The timeless classic console space adventure between space foes.")
+alert(`Captain Space Cat... There are ${alienShips.length} alien ships headed your way, but you can only fight one at a time! Start reading your browser console and ready up for battle!`)
 
-// ussSpaceCat.retreat();
+// Function to capture input, then use that input to attack
+
+gameWon(); // calls the game won function
