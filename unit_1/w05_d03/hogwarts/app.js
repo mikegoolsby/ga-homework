2a705b6ab10b0bdc258fbc73103ea37bd2ba7b8a
Michael Goolsby <mgoolsby1091@gmail.com>
2020-08-12 10:36:35 -0400

    adds more elements

--- unit_1/w05_d03/hogwarts/app.js
+++ unit_1/w05_d03/hogwarts/app.js
@@ -5,4 +5,18 @@ $(() => {
     // console.log($h1)
     $h1.text("Hogwarts");
     $h1.appendTo($container);
+    const $h2 = $('<h2>');
+    $h2.text("Mike");
+    $h2.appendTo($container);
+    const $h3 = $('<h3>');
+    $h3.text("Gryffindor");
+    $h3.appendTo($container);
+    const $h4 = $('<h4>')
+    $h4.addClass('owl');
+    $h4.text("Louie");
+    $h4.appendTo($container);
+    const $h4Wand = $('<h4>');
+    $h4Wand.text("Hornbeam Wand with Dragon Heartstring Core");
+    $h4Wand.appendTo($container);
+
 });
