577414fa9d8d81b40d6d19455a9796b580799637
Michael Goolsby <mgoolsby1091@gmail.com>
2020-08-17 11:43:38 -0400

    finishes ch 6

--- unit_1/w06_d02/lotr_lite/scripts/fellowship.js
+++ unit_1/w06_d02/lotr_lite/scripts/fellowship.js
@@ -162,6 +162,8 @@ const makeBuddies = () => {
 const leaveTheShire = () => {
   // 1. grab the hobbits (the ul in which they reside) and move them to Rivendell
       // hint: the hobbits ul is a childNode of The-Shire-- there is way to get a list of childNodes
+    const $moveHobbits = $('ul').children()
+    $('#Rivendell').append($moveHobbits);
 
   //Stretch: add an event handler/listener so that when you click on the `h1` The Shire, this function will be called (be sure to do it in the window.onload/document.ready function)
 
