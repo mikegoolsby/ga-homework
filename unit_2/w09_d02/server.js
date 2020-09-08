5165f409821228880104136889850d054a30a1d5
Michael Goolsby <mgoolsby1091@gmail.com>
2020-09-08 11:33:27 -0400

    adds 8 ball

--- unit_2/w09_d02/server.js
+++ unit_2/w09_d02/server.js
@@ -16,6 +16,16 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`Please leave $${tip} as a tip`)
 })
 
+const magicEightBallResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get('/magic/:phrase', (req, res) => {
+    let randomRes = Math.floor(Math.random() * magicEightBallResponses.length)
+    res.send(`
+    ${req.params.phrase}
+    <h1>${magicEightBallResponses[randomRes]}<h1>
+    `)
+})
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
