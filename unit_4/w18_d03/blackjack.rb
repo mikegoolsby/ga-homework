9f93f16d23ef98edb978130e71256f6537eefe94
Michael Goolsby <mgoolsby1091@gmail.com>
2020-11-18 19:47:49 -0500

    adds game logic

--- unit_4/w18_d03/blackjack.rb
+++ unit_4/w18_d03/blackjack.rb
@@ -45,3 +45,53 @@ deck.shuffle!
 
 #entering logic below
 
+quit = false
+puts "Player's name: "
+name = gets.chomp #takes above line and enters it as variable 'name'
+player = Human.new(name)
+computer = Computer.new("The House")
+
+while !quit
+    if player.bankroll <= 0
+        puts "You ran out of money. You lose"
+        quit = true
+        break
+    end
+    puts "#{player.name} is literally #{player.bankroll} deep in the bank"
+    puts "#{computer.name} (cheater) is literally #{computer.bankroll} in the bank and prob in the 1%"
+    puts "(Q)uit, or (D)eal? LMK..."
+    input = gets.chomp
+    if input == "d"
+        player.hand = deck.sample(2)
+        computer.hand = deck.sample(2)
+        player.sum(player.hand)
+        computer.sum(computer.hand)
+        puts "you're stackin: #{player.sum_hand} and the computer (cheater) is stackin: #{computer.sum_hand}"
+        if player.sum_hand == 21
+            player.bankroll += 10
+            computer.bankroll -= 10 
+            puts "You win! Wawaweewa!"
+        elsif player.sum_hand > 21
+            computer.bankroll += 10
+            player.bankroll -= 10
+            puts "house wins, you lose. woof."
+        else
+
+            if player.sum_hand > computer.sum_hand
+                player.bankroll += 10
+                computer.bankroll -= 10 
+                puts "Wawaweewa! 10 dollars from the house man!"
+            elsif computer.sum_hand > player.sum_hand
+                computer.bankroll += 10
+                player.bankroll -= 10
+                puts "It is rigged against you and the house won again!"
+            else 
+                puts "It's a tie! 2020 strikes again and congress must choose the winner here."
+            end
+        end
+    elsif input == 'q'
+        quit = true
+    else 
+        puts "Invalid input. Please read the instructions..."
+    end
+end
