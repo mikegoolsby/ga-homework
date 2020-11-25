e1a3bcfe275118d5371ea6a99d8521e2d1568cd6
Michael Goolsby <mgoolsby1091@gmail.com>
2020-11-24 21:54:38 -0500

    tweet index and tweet show

--- unit_4/w20_d03/tweeter_app_api/app/controllers/tweets_controller.rb
+++ unit_4/w20_d03/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -1,9 +1,12 @@
 class TweetsController < ApplicationController
 
     def index
+        render json: Tweet.all
     end
 
     def show
+        tweet = Tweet.find(params[:id])
+        render(json: { tweet: tweet })
     end
 
 end
